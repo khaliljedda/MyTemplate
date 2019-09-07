7b4bf5a278827d01e1adfe92168e1e121b2983c4
khaliljedda <khaliljedda4@gmail.com>
2019-09-07 10:53:38 +0200

    final

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ 
+    $("#page").slideDown("slow");
+  
+});
+$(document).ready(function(){
+	$(".portfolio-link").click(function(){
+		$("#portfolioModal3").modal('show');
+	});
+});
---
+++ js/skils.js
@@ -0,0 +1,136 @@
+$(document).ready(function(){$("#1").mouseover(function(){
+    $("#1").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#1"). mouseleave(function(){
+    $("#1").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+}); 
+
+
+               
+  $(document).ready(function(){$("#2").mouseover(function(){
+    $("#2").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#2"). mouseleave(function(){
+    $("#2").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+}); 
+
+
+               
+  $(document).ready(function(){$("#3").mouseover(function(){
+    $("#3").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#3"). mouseleave(function(){
+    $("#3").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+}); 
+
+
+               
+  $(document).ready(function(){$("#4").mouseover(function(){
+    $("#4").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#4"). mouseleave(function(){
+    $("#4").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+}); 
+
+
+               
+  $(document).ready(function(){$("#5").mouseover(function(){
+    $("#5").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#5"). mouseleave(function(){
+    $("#5").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+}); 
+
+
+               
+  $(document).ready(function(){$("#6").mouseover(function(){
+    $("#6").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#6"). mouseleave(function(){
+    $("#6").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+});
+
+
+          $(document).ready(function(){$("#7").mouseover(function(){
+    $("#7").animate({
+
+      height: '130px',
+      width: '130px'
+    });
+  }); 
+});
+
+        $(document).ready(function(){$("#7"). mouseleave(function(){
+    $("#7").animate({
+
+      height: '120px',
+      width: '120px'
+    });
+  });
+}); 
